0aad758addb2b821b347babd9d6e042afaa1c388
Kei Mizubuchi <41995658+Kmzbch@users.noreply.github.com>
2020-10-22 12:09:53 -0400

    added Angular Material table

--- src/app/pages/container/forms/forms.component.ts
+++ src/app/pages/container/forms/forms.component.ts
@@ -1,41 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+// reference:
+// https://material.angular.io/components/table/overview
+
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface IForm {
+  filename: string;
+  creation_date: string;
+  size: number;
+}
+
+const ELEMENT_DATA: IForm[] = [
+  {
+    filename: 'A',
+    creation_date: '2020-01-01',
+    size: 1024
+  },
+  {
+    filename: 'B',
+    creation_date: '2020-01-01',
+    size: 1024
+  }
+];
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent implements OnInit {
+export class FormsComponent implements AfterViewInit  {
+  displayedColumns: string[] = ['filename', 'creation_date', 'size'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  // public formList: Forms[] = []
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
   public formList: Object[] = [];
   public searchQuery: string;
 
   constructor() {
+
   }
 
-  ngOnInit(): void {
-    this.formList = [
-            {
-                filename: 'A',
-                creation_date: '2020-01-01',
-                size: 1024
-            },
-            {
-                filename: 'A',
-                creation_date: '2020-01-01',
-                size: 1024
-                }
-    ];
-  }
-
-//
   public clickUpload() {
-      alert();
-      // showUploadDialog();
-    }
-
-  showUploadDialog(){
+    alert();
+    // showUploadDialog();
   }
 
 }
