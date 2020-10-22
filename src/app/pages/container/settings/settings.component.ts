31a3dd21201cb026d710617c93a2153b5ba5ec63
Sadia Rashid <srashi25@my.centennialcollege.ca>
2020-10-22 14:33:19 -0400

    logo page created

--- src/app/pages/container/settings/settings.component.ts
+++ src/app/pages/container/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {StaffManagementComponent} from './staff-management/staff-management.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  openStaffManagementDialog() {
+    const dialogRef = this.dialog.open(StaffManagementComponent, {
+      maxWidth: 'auto',
+      maxHeight: 'auto',
+      height: '100%',
+      width: '100%',
+      panelClass: 'no-padding-container',
+      data: {}
+    });
+    // dialogRef.componentInstance.onAdd.subscribe(result => {
+    //   console.log('AfterAdd dialog', JSON.stringify(result));
+    //
+    // });
+    // dialogRef.afterClosed().subscribe(result => {
+    //
+    //   }
+    // });
+  }
+
 
 }
--- src/app/pages/container/settings/staff-management/add-edit-staff/add-edit-staff.component.ts
+++ src/app/pages/container/settings/staff-management/add-edit-staff/add-edit-staff.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-edit-staff',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditStaffComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<AddEditStaffComponent>) { }
 
   ngOnInit(): void {
   }
-
+  /** Clicking on close */
+  close() {
+    // if (this.addMedicationForm.dirty) {
+    //   const unsavedDialog = this.dialog.open(PendingChangesDialogComponent);
+    //
+    //   unsavedDialog.afterClosed().subscribe(res => {
+    //     if (res === true) {
+    //       this.dialogRef.close(false);
+    //     }
+    //   });
+    // } else {
+    //   this.dialogRef.close(false);
+    // }
+    this.dialogRef.close();
+  }
 }
