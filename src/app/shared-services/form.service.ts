e03adbe244d68c0a05f127cc9dbe3686b684cc45
Kei Mizubuchi <41995658+Kmzbch@users.noreply.github.com>
2020-11-06 02:31:19 -0500

    Implemented getall forms

--- src/app/shared-services/form.service.ts
+++ src/app/shared-services/form.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import {AngularFireDatabase, AngularFireList} from "@angular/fire/database";
 import {FirebaseFormsModel} from "../pages/container/forms/manager/firebase-forms.model";
+import {FirebasePropertiesModel} from "../pages/container/property-list/manager/firebase-properties.model";
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
   private dbPath = '/form'
-  private propId ="M3Xe58nTHXxnaM6Mp-m";
-  formsRef: AngularFireList<FirebaseFormsModel> = null;
+  // private propId ="M3Xe58nTHXxnaM6Mp-m";
 
-  constructor(private db: AngularFireDatabase) {
-    this.formsRef = db.list(this.dbPath, ref =>
-      ref.orderByChild('phone').equalTo(this.propId));
+  formsRef: AngularFireList<FirebaseFormsModel> = null;
+  // formsRefCreate: AngularFireList<FirebaseFormsModel> = null;
+  propId;
+
+  constructor(private db: AngularFireDatabase,
+              private authService: AuthService) {
+    this.propId = this.authService.GetUserInSession().propId;
+
+    this.formsRef = db.list(this.dbPath);
+
+    // this.formsRefCreate = db.list(this.dbPath);
   }
 
   getAll(): AngularFireList<FirebaseFormsModel> {
