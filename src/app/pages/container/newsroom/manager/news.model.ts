5a5ec98e816847f15c11c59c89e069eebfff2ba1
Hang (Anna) Li <annlihappy@gmail.com>
2020-11-07 11:08:56 -0500

    newsroom landing page

---
+++ src/app/pages/container/newsroom/manager/news.model.ts
@@ -0,0 +1,10 @@
+export class NewsModel {
+  key: string;
+  propId: string;
+  title: string;
+  content: string;
+  imageUrl: string;
+  createdDate: string;
+  isHidden: boolean;
+  viewGroup: string;
+}
--- src/app/pages/container/newsroom/newsroom.component.ts
+++ src/app/pages/container/newsroom/newsroom.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {AddEditNewsDialogComponent} from './add-edit-news-dialog/add-edit-news-dialog.component';
+import {NewsModel} from './manager/news.model';
 
 @Component({
   selector: 'app-newsroom',
@@ -7,32 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsroomComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  newsList = [];
+  news: NewsModel;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  public openAddNewsDialog(){
+    const dialogFilter = this.dialog.open(AddEditNewsDialogComponent, {
+      height: '400px',
+      width: '850px',
+      disableClose: true
+    });
+  }
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
